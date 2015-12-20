72327aa7ed32651cb0826e7dd17998b34568d581
brutalenemy666 <brutalenemy666@gmail.com>
2015-12-20 12:14:03 +0200

    batch script to redirect windows port 80 to 8080 and 443 to 8443 when using a vagrant

---
+++ batch/vagrant-ports.bat
@@ -0,0 +1,2 @@
+netsh interface portproxy add v4tov4 listenport=80 connectport=8080 connectaddress=127.0.0.1
+netsh interface portproxy add v4tov4 listenport=443 connectport=8443 connectaddress=127.0.0.1
