25ee2c0cbeb5c889cfcefbd23b23a161fda198fe
brutalenemy666 <brutalenemy666@gmail.com>
2016-01-31 23:21:10 +0200

    adding a composer / laravel installers

--- bash/server-setup.sh
+++ bash/server-setup.sh
@@ -1,20 +1,45 @@
-# apache
+#!/bin/bash
+
+echo "#------------------------------"
+echo "Adding ppa repositories for Apache2 and PHP7"
+echo "#------------------------------"
 sudo add-apt-repository ppa:ondrej/apache2
+sudo add-apt-repository ppa:ondrej/php-7.0
 sudo apt-get update
+
+echo "#------------------------------"
+echo "Installing apache2"
+echo "#------------------------------"
 sudo apt-get insall apache2
 
-# install mysql
+echo "#------------------------------"
+echo "MySQL"
+echo "#------------------------------"
 sudo apt-get install mysql-server --fix-missing
 # mysql -uroot -hlocalhost -p
 
-# php 7
+echo "#------------------------------"
+echo "Installing PHP7"
+echo "#------------------------------"
 sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:ondrej/php-7.0
-sudo apt-get update
 sudo apt-get install php7.0 php7.0-common php7.0-cgi php7.0-fpm php7.0-mysql
 
-# list available modules
+echo "#------------------------------"
+echo "PHP7 available modules"
+echo "#------------------------------"
 sudo apt-cache search php7-*
 
-# install some requirements
+echo "#------------------------------"
+echo "Installing php7/apache requirements"
+echo "#------------------------------"
 sudo apt-get install libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json
+
+echo "#------------------------------"
+echo "Installing composer"
+echo "#------------------------------"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+echo "#------------------------------"
+echo "Install Laravel"
+echo "#------------------------------"
+composer global require "laravel/installer"
