81ef9315434b9d6f9636862474a2668443e733cc
brutalenemy666 <brutalenemy666@gmail.com>
2016-01-31 22:46:32 +0200

    script for automated installation of apache, mysql and php7

---
+++ bash/server-setup.sh
@@ -0,0 +1,10 @@
+# apache
+sudo apt-get insall apache2
+
+# install mysql
+sudo apt-get install mysql-server --fix-missing
+# mysql -uroot -hlocalhost -p
+
+# php 7
+sudo add-apt-repository ppa:ondrej/php-7.0
+sudo apt-get install php7.0 php7.0-common php7.0-cgi php7.0-fpm php7.0-mysql
