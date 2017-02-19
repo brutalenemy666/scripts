5a37dd8ccc6df6dea8a152588d47816440ddf4bf
Rashko Petrov <brutalenemy666@gmail.com>
2017-02-19 19:15:54 +0200

    Add bash script to forward the traffic from port to port

---
+++ bash/iptables-forward-port-traffic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pattern=^[0-9]+$
+
+while ! [[ $port_source =~ $pattern ]]; do
+    echo "Enter source port."
+    read port_source
+done
+
+while ! [[ $port_target =~ $pattern ]]; do
+    echo "Enter target port."
+    read port_target
+done
+
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport $port_source -j REDIRECT --to-port $port_target
+
+echo "The traffic from port $port_source to port $port_source is now forwarded."
