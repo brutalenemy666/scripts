2d7269794d1c2bbb256860ae247a63ed216d9d56
Rashko Petrov <brutalenemy666@gmail.com>
2017-02-20 13:32:54 +0200

    Fix typos

--- bash/iptables-forward-port-traffic.sh
+++ bash/iptables-forward-port-traffic.sh
@@ -14,4 +14,4 @@ done
 
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport $port_source -j REDIRECT --to-port $port_target
 
-echo "The traffic from port $port_source to port $port_source is now forwarded."
+echo "The traffic from port $port_source to port $port_target is now forwarded."
